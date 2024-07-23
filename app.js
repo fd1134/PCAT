f87eace05b5b6bacebe3fb566c256504fe23dece
Fatih Demir <fd1134@gmail.com>
2024-07-23 18:31:01 +0300

    static file upluad

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const express=require("express");
+const express = require('express');
+const path=require("path");
 
-const app=express();
+const app = express();
 
-app.get("/",(req,res)=>{
-    
-    res.send("bs");
 
+//Middleware
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+ //res.sendFile(path.resolve("./temp/index.html")); 
+ res.sendFile(path.resolve(__dirname,"temp/index.html"));
 });
-const port=3000;
+const port = 3000;
 
-app.listen(port,()=>{
-    console.log(`Sunucu ${port} portunda başlatıldı...`);
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı...`);
 });
